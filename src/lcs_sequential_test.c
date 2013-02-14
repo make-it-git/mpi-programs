1a351edfd24eac37a0db13830bc26f4cbd5c88ef
make.it.git <make.it.git@gmail.com>
2013-02-14 10:05:28 +0400

    new test (if no lcs found)

--- src/lcs_sequential_test.c
+++ src/lcs_sequential_test.c
@@ -57,12 +57,31 @@ START_TEST(test_lcs3)
     free(res_matr);
 END_TEST
 
+START_TEST(test_no_lcs)
+    char *str1 = "jgwa";
+    char *str2 = "lopxcz";
+    int str1_len = strlen(str1);
+    int str2_len = strlen(str2);
+    int **res_matr = malloc(sizeof(void*) * (str1_len + 1));
+    int i;
+    for(i = 0; i <= str1_len; i++)
+        res_matr[i] = malloc(sizeof(int) * (str2_len + 1));
+    int lcs_len = lcs_length(res_matr, str1, str1_len, str2, str2_len);
+    ck_assert(lcs_len == 0);
+    char *seq = lcs_sequence(res_matr, lcs_len, str1, str2);
+    ck_assert_str_eq(seq, "");
+    for(i = 0; i <= str1_len; i++)
+        free(res_matr[i]);
+    free(res_matr);
+END_TEST
+
 Suite *lcs_suite(void) {
     Suite *s = suite_create("LCS");
     TCase *tc_core = tcase_create("Core");
     tcase_add_test(tc_core, test_lcs1);
     tcase_add_test(tc_core, test_lcs2);
     tcase_add_test(tc_core, test_lcs3);
+    tcase_add_test(tc_core, test_no_lcs);
     suite_add_tcase(s, tc_core);
     return s;
 }
