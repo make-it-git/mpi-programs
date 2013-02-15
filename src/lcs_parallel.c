adcf18ef1b219633aa6c7592bad3878927e0b391
make.it.git <make.it.git@gmail.com>
2013-02-15 23:40:46 +0400

    Added parallel processing

--- src/lcs_parallel.c
+++ src/lcs_parallel.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 #include "lcs_functions.h"
+#include <unistd.h>
+
+#define BUF_SIZE (1024*1024)
 
 int main(int argc, char **argv) {
-    char *str1 = "TAGCA";
+    /*//char *str1 = "TAGCA";
+    char *str1 = "EMPTYBOTTLE";
     int str1_len = strlen(str1);
-    char *str2 = "ATTGCCA";
+    //char *str2 = "ATTGCCA";
+    char *str2 = "NEMATODEKNOWLEDGE";
     int str2_len = strlen(str2);
-    char *alphabet = "ATCG";
-    int alp_len = strlen(alphabet);
+    //char *alphabet = "ATCG";
+    char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int alp_len = strlen(alphabet);*/
     MPI_Init(&argc, &argv);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    char *str1 = malloc(sizeof(char) * BUF_SIZE);
+    char *str2 = malloc(sizeof(char) * BUF_SIZE);
+    int str1_len;
+    int str2_len;
+    char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int alp_len = strlen(alphabet);
+    if(rank == 0) {
+        char *c;
+        FILE *f1 = fopen(argv[1], "rb");
+        FILE *f2 = fopen(argv[2], "rb");
+        fgets(str1, BUF_SIZE, f1);
+        fgets(str2, BUF_SIZE, f2);
+        c = strchr(str1, '\n');
+        if(c) *c = '\0';
+        c = strchr(str2, '\n');
+        if(c) *c = '\0';
+        str1_len = strlen(str1);
+        str2_len = strlen(str2);
+        fclose(f1);
+        fclose(f2);
+    }
+    MPI_Bcast(&str1_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&str2_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(str1, str1_len, MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Bcast(str2, str2_len, MPI_CHAR, 0, MPI_COMM_WORLD);
     int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     size -= 1;
@@ -39,6 +70,7 @@ int main(int argc, char **argv) {
     for(i = 0; i < alp_len; i++) {
         MPI_Bcast(P[i], str2_len+1, MPI_INT, 0, MPI_COMM_WORLD);
     }
+    //sleep(8);
     MPI_Barrier(MPI_COMM_WORLD);
     if(rank == 0) {
         int **S = malloc(sizeof(void*) * (str1_len + 1));
@@ -58,23 +90,27 @@ int main(int argc, char **argv) {
             }
             MPI_Bcast(S[i], str2_len+1, MPI_INT, 0, MPI_COMM_WORLD);
         }
-        for(i = 0; i <= str1_len; i++) {
+        //printf("%s\n", lcs_sequence(S, S[str1_len][str2_len], str1, str1_len, str2, str2_len));
+        /*for(i = 0; i <= str1_len; i++) {
             for(j = 0; j <= str2_len; j++) {
                 printf("%d\t", S[i][j]);
             }
             printf("\n");
-        }
+        }*/
         for(i = 0; i <= str1_len; i++) {
             free(S[i]);
         }
         free(S);
         free(s_buf);
     } else { //rank>0
+        //sleep(8);
         parallel_calc_S(P, alphabet, str1, str1_len, str2_len);
     }
     for(i = 0; i < alp_len; i++)
         free(P[i]);
     free(P);
+    /*free(str1);
+    free(str2);*/
     MPI_Finalize();
     return 0;
 }
--- src/lcs_sequential.c
+++ src/lcs_sequential.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include "lcs_functions.h"
 
-int main() {
+#define BUF_SIZE (1024*1024)
+
+int main(int argc, char **argv) {
+    char *str1 = malloc(sizeof(char) * BUF_SIZE);
+    char *str2 = malloc(sizeof(char) * BUF_SIZE);
+    int str1_len;
+    int str2_len;
+    char *c;
+    FILE *f1 = fopen(argv[1], "rb");
+    FILE *f2 = fopen(argv[2], "rb");
+    fgets(str1, BUF_SIZE, f1);
+    fgets(str2, BUF_SIZE, f2);
+    c = strchr(str1, '\n');
+    if(c) *c = '\0';
+    c = strchr(str2, '\n');
+    if(c) *c = '\0';
+    str1_len = strlen(str1);
+    str2_len = strlen(str2);
+    fclose(f1);
+    fclose(f2);
+    int **res_matr = malloc(sizeof(void*) * (str1_len + 1));
+    int i;
+    for(i = 0; i <= str1_len; i++)
+        res_matr[i] = malloc(sizeof(int) * (str2_len + 1));
+    int lcs_len = lcs_length(res_matr, str1, str1_len, str2, str2_len);
+    printf("%d\n", lcs_len);
+    char *seq = lcs_sequence(res_matr, lcs_len, str1, str1_len, str2, str2_len);
+    printf("%d\n", lcs_len);
+    free(seq);
+    for(i = 0; i <= str1_len; i++)
+        free(res_matr[i]);
+    free(res_matr);
+    free(str1);
+    free(str2);
     return 0;
 }
