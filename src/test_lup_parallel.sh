ced4d3e6412f21c62beb3df3824e742740af0fa8
make.it.git <make.it.git@gmail.com>
2013-03-01 20:20:58 +0400

    Added test which compares results from sequential and parallel version of LUP decomposition

--- src/test_lup_parallel.sh
+++ src/test_lup_parallel.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#N=( 5 10 12 15 17 25 30 50 100 )
-#PROC_COUNT=( 2 3 4 5 10 15 )
-#TMP_MATR="/tmp/M"
-#for S in ${N[@]}; do
-#    ./bin/lup_generate_matrix $TMP_MATR $S
-#    echo size=$S
-#    #./bin/lup_sequential "$TMP_MATR" $S >! /tmp/seq
-#    for i in ${PROC_COUNT[@]}; do
-#        #mpirun -np $i ./bin/lup_parallel "$TMP_MATR" $S >! /tmp/par
-#        echo $i
-#        #diff /tmp/seq /tmp/par
-#    done 
-#done
+N=( 5 10 12 15 17 25 30 50 100 )
+PROC_COUNT=( 2 3 4 5 10 15 )
+TMP_MATR="/tmp/Matrix"
+TMP_SEQ="/tmp/Sequential"
+TMP_PAR="/tmp/Parallel"
+echo Testing...
+echo \'diff\' output indicates problems
+for S in ${N[@]}; do
+    ./bin/lup_generate_matrix $TMP_MATR $S
+    echo Testing size = $S
+    ./bin/lup_sequential "$TMP_MATR" "$S" "$TMP_SEQ"
+    for i in ${PROC_COUNT[@]}; do
+        mpirun -np $i ./bin/lup_parallel "$TMP_MATR" "$S" "$TMP_PAR"
+        echo Number of processes = $i
+        diff "$TMP_SEQ" "$TMP_PAR"
+    done 
+done
